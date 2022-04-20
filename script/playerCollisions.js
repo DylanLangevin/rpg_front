66ae3cad1a3180814b4012d9be4780b7b5dad206
DylanLangevin <dylan.lgvn@gmail.com>
2022-04-20 10:43:30 +0200

    commit

--- script/playerCollisions.js
+++ script/playerCollisions.js
@@ -272,7 +272,7 @@ function checkAllDialogueCollisions() {
 function checkAllItemCollisions() {
     mapsItemCollisions[currentMap].forEach(element => {
         if((player.position.x + offsetX + hitboxWidth > element.x && player.position.x + offsetX < element.x + element.width && player.position.y + offsetY + hitboxHeight >  element.y && player.position.y + offsetY < element.y + element.height)) {
-
+            console.log("objet recupéré");
         }
     });
 }
