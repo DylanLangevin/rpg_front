94f931111c6898a5ecb62a617c8d1e125e595f88
Doud75 <adrienquimbre@gmail.com>
2022-04-22 11:47:36 +0200

    fix hit box dialog

--- script/initDialogueCollisions.js
+++ script/initDialogueCollisions.js
@@ -7,28 +7,31 @@ let cityDialogueCollisions = [
 let coffeeDialogueCollisions = [
     // Pnj Bar
     {x:maleCitizen.position[0], y:maleCitizen.position[1], width: 50, height: 120, pnj: maleCitizen, dialogue: "yo broooo"},
-    {x:elder.position[0], y:elder.position[1], width: 50, height: 120, pnj: elder, dialogue: "yo broooo"},
+    {x:elder.position[0], y:elder.position[1], width: 50, height: 80, pnj: elder, dialogue: "yo broooo"},
 ]
 
 let parcRightMapDialogueCollisions = [
-    {x:engineer.position[0], y:engineer.position[1], width: 50, height: 120, pnj: engineer, dialogue: "yo broooo"},
+    {x:engineer.position[0], y:engineer.position[1], width: 50, height: 80, pnj: engineer, dialogue: "yo broooo"},
+    {x:plantJanitor.position[0] -30, y:plantJanitor.position[1], width: 80, height: 50, pnj: plantJanitor, dialogue: "yo broooo"},
 ];
 let parcLeftMapDialogueCollisions = [
-    {x:mayorWife.position[0], y:mayorWife.position[1], width: 50, height: 120, pnj: mayorWife, dialogue: "yo broooo"},
+    {x:mayorWife.position[0], y:mayorWife.position[1] -30, width: 50, height: 80, pnj: mayorWife, dialogue: "yo broooo"},
+    {x:secretary.position[0], y:secretary.position[1], width: 80, height: 50, pnj: secretary, dialogue: "yo broooo"},
 ];
 let parcMapDialogueCollisions = [
     // pnj piscine parc
-    {x:femaleCitizen.position[0], y:femaleCitizen.position[1], width: 50, height: 120, pnj: femaleCitizen, dialogue: "yo broooo"},
+    {x:femaleCitizen.position[0], y:femaleCitizen.position[1], width: 50, height: 80, pnj: femaleCitizen, dialogue: "yo broooo"},
+    {x:childhoodFriend.position[0] -30, y:childhoodFriend.position[1], width: 80, height: 50, pnj: childhoodFriend, dialogue: "yo broooo"},
 ];
 let libraryMapDialogueCollisions = [
     // Message porte fermé
     {x:945, y:440, width: 16, height: 70, pnj: libraryBackDoor, dialogue:"Nécessite une clé pour ouvrir"},
-    {x:librarian.position[0], y:librarian.position[1], width: 50, height: 120, pnj: librarian, dialogue: "yo broooo"},
+    {x:librarian.position[0] -30, y:librarian.position[1], width: 80, height: 50, pnj: librarian, dialogue: "yo broooo"},
 
 ];
 
 let librarySecondMapDialogueCollisions = [
-    {x:reader.position[0], y:reader.position[1], width: 50, height: 120, pnj: reader, dialogue: "yo broooo"},
+    {x:reader.position[0] -30, y:reader.position[1], width: 80, height: 50, pnj: reader, dialogue: "yo broooo"},
 ];
 
 let mapsDialogueCollisions = [cityDialogueCollisions, coffeeDialogueCollisions, parcRightMapDialogueCollisions, parcLeftMapDialogueCollisions, parcMapDialogueCollisions, libraryMapDialogueCollisions, librarySecondMapDialogueCollisions]
--- script/initialize_class.js
+++ script/initialize_class.js
@@ -1,17 +1,17 @@
 // Initialisation des objets pnj
-let secretary = new Pnj("Secrétaire", 'character_profile/secretary.png', [12, 126]);//position a définir
-let barRegular = new Pnj("Habitué du bar", 'character_profile/bar_regular.png', [300, 300]);//position a définir
-let engineer = new Pnj("Ingénieur fou", 'character_profile/engineer.png', [400, 250]);//position a définir
-let childhoodFriend = new Pnj("Ami d'enfance", 'character_profile/childhood_friend.png', [12, 126]);//position a définir
-let plantJanitor = new Pnj("Gardien d'usine", 'character_profile/plant_janitor.png', [12, 126]);//position a définir
-let elder = new Pnj("Ancien du village", 'character_profile/elder.png', [600, 226]);//position a définir
-let mayorWife = new Pnj("Femme du maire", 'character_profile/mayor_wife.png', [130, 126]);//position a définir
-let foreigner = new Pnj("L'étrangère", 'character_profile/foreigner.png', [12, 126]);//position a définir
-let reader = new Pnj("Lectrice de polar", 'character_profile/reader.png', [800, 376]);//position a définir
-let librarian = new Pnj("Bibliothécaire", 'character_profile/librarian.png', [300, 456]);//position a définir
-let policeFriend = new Pnj("Policier ami", 'character_profile/police_friend.png', [12, 126]);//position a définir
-let maleCitizen = new Pnj("Villageois", 'character_profile/male_citizen.png', [180, 156]);//position a définir
-let femaleCitizen = new Pnj("Villageoise", 'character_profile/female_citizen.png', [350, 150]);//position a définir
+let secretary = new Pnj("Secrétaire", 'character_profile/secretary.png', [130, 126]);
+let barRegular = new Pnj("Habitué du bar", 'character_profile/bar_regular.png', [300, 300]);
+let engineer = new Pnj("Ingénieur fou", 'character_profile/engineer.png', [400, 250]);
+let childhoodFriend = new Pnj("Ami d'enfance", 'character_profile/childhood_friend.png', [675, 475]);
+let plantJanitor = new Pnj("Gardien d'usine", 'character_profile/plant_janitor.png', [850, 375]);
+let elder = new Pnj("Ancien du village", 'character_profile/elder.png', [600, 226]);
+let mayorWife = new Pnj("Femme du maire", 'character_profile/mayor_wife.png', [675, 450]);
+let foreigner = new Pnj("L'étrangère", 'character_profile/foreigner.png', [12, 126]);
+let reader = new Pnj("Lectrice de polar", 'character_profile/reader.png', [800, 376]);
+let librarian = new Pnj("Bibliothécaire", 'character_profile/librarian.png', [300, 456]);
+let policeFriend = new Pnj("Policier ami", 'character_profile/police_friend.png', [12, 126]);
+let maleCitizen = new Pnj("Villageois", 'character_profile/male_citizen.png', [180, 156]);
+let femaleCitizen = new Pnj("Villageoise", 'character_profile/female_citizen.png', [350, 150]);
 
 // (pnj porte pour dialogue)
 let libraryBackDoor = new Pnj("Arrière bibliothèque", '', [750, 430])
@@ -24,20 +24,27 @@ function pnjCityPosition() {
 
 function pnjCoffeePosition() {
     ctxBackground.drawImage(maleCitizen.character, 0, 122,frameWidth*scaleDivider, frameHeight*scaleDivider, maleCitizen.position[0], maleCitizen.position[1], frameWidth, frameHeight)
+
     ctxBackground.drawImage(elder.character, 0, 130,frameWidth, frameHeight, elder.position[0], elder.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
 }
 
 function pnjParcPosition() {
     ctxBackground.drawImage(femaleCitizen.character, 0, 130,frameWidth, frameHeight, femaleCitizen.position[0], femaleCitizen.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
+
+    ctxBackground.drawImage(childhoodFriend.character, 0, 64,frameWidth, frameHeight, childhoodFriend.position[0], childhoodFriend.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
 }
 
 function pnjParcRightPosition() {
     ctxBackground.drawImage(engineer.character, 0, 128,frameWidth, frameHeight, engineer.position[0], engineer.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
 
+    ctxBackground.drawImage(plantJanitor.character, 0, 64,frameWidth, frameHeight, plantJanitor.position[0], plantJanitor.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
+
 }
 
 function pnjParcLeftPosition() {
-    ctxBackground.drawImage(mayorWife.character, 0, 192,frameWidth, frameHeight, mayorWife.position[0], mayorWife.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
+    ctxBackground.drawImage(mayorWife.character, 0, 0,frameWidth, frameHeight, mayorWife.position[0], mayorWife.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
+
+    ctxBackground.drawImage(secretary.character, 0, 192,frameWidth, frameHeight, secretary.position[0], secretary.position[1], frameWidth/scaleDivider, frameHeight/scaleDivider)
 }
 
 function pnjLibraryFirstMapPosition() {
