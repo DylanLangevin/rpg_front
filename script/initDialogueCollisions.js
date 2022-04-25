1cced4543215e78a94151acb4eef12abdc2b80c0
Louis R <95973810+LouisSleep@users.noreply.github.com>
2022-04-25 19:06:09 +0200

    Update initDialogueCollisions.js

--- script/initDialogueCollisions.js
+++ script/initDialogueCollisions.js
@@ -1,17 +1,19 @@
 
 let cityDialogueCollisions = [
+
+
 ];
 
 let coffeeDialogueCollisions = [
     // Pnj Bar
-    {x:maleCitizen.position[0], y:maleCitizen.position[1], width: 50, height: 120, pnj: maleCitizen, dialogue: "Devine la réponse. Je suis grand, j’ai des dents noires et blanches… Touche-les et fait raisonner les clefs"},
-    {x:elder.position[0], y:elder.position[1], width: 50, height: 80, pnj: elder, dialogue: "Peronne ne veut sortir de chez soi aujourd'hui ! Le centre-ville est vide."},
-    {x:policeFriend.position[0], y:policeFriend.position[1], width: 80, height: 50, pnj: policeFriend, dialogue: "Trouve les 5 indices que t'as laissé ! Je t'aiderai à fuir."},
-    {x:foreigner.position[0], y:foreigner.position[1] -30, width: 50, height: 80, pnj: foreigner, dialogue: "Les habitants semble suspécieux à mon égard! Seulement parce que je viens pas d'ici..."},
+    {x:maleCitizen.position[0], y:maleCitizen.position[1], width: 50, height: 120, pnj: maleCitizen, dialogue: "Devine la réponse. Je suis grand, j’ai des dents noir et blanc… Touche-les et fais raisonner les clefs."},
+    {x:elder.position[0], y:elder.position[1], width: 50, height: 80, pnj: elder, dialogue: "Personne ne veut sortir de chez soi aujourd'hui ! Le centre-ville est vide."},
+    {x:policeFriend.position[0], y:policeFriend.position[1], width: 80, height: 50, pnj: policeFriend, dialogue: "Trouve les 5 indices que t'as laissés ! Je t'aiderai à fuir."},
+    {x:foreigner.position[0], y:foreigner.position[1] -30, width: 50, height: 80, pnj: foreigner, dialogue: "Les habitants semblent suspicieux à mon égard ! Seulement parce que je ne viens pas d'ici..."},
 ]
 
 let parcRightMapDialogueCollisions = [
-    {x:engineer.position[0], y:engineer.position[1], width: 50, height: 80, pnj: engineer, dialogue: "AHAHAH ! En cueillant ces belles marguerites d'automne, il y avait un couteau plein de sang au sol!"},
+    {x:engineer.position[0], y:engineer.position[1], width: 50, height: 80, pnj: engineer, dialogue: "AHAHAH ! En cueillant ces belles marguerites d'automne, il y avait un couteau plein de sang au sol !"},
     {x:plantJanitor.position[0] -30, y:plantJanitor.position[1], width: 80, height: 50, pnj: plantJanitor, dialogue: "J'ai cru comprendre que les officiers avaient des pistes dans la bibliothèque."},
 ];
 let parcLeftMapDialogueCollisions = [
@@ -20,12 +22,12 @@ let parcLeftMapDialogueCollisions = [
 ];
 let parcMapDialogueCollisions = [
     // pnj piscine parc
-    {x:femaleCitizen.position[0], y:femaleCitizen.position[1], width: 50, height: 80, pnj: femaleCitizen, dialogue: "Ras-le bol des gens sales dans cette ville ! il y a une bouteille près du pont. C'est dégoutant !"},
-    {x:childhoodFriend.position[0] -30, y:childhoodFriend.position[1], width: 80, height: 50, pnj: childhoodFriend, dialogue: "Je sais ce que tu as fait ! tu n'as pas honte ?"},
+    {x:femaleCitizen.position[0], y:femaleCitizen.position[1], width: 50, height: 80, pnj: femaleCitizen, dialogue: "Ras-le-bol des gens sales dans cette ville ! Il y a une bouteille près du pont. C'est dégoutant !"},
+    {x:childhoodFriend.position[0] -30, y:childhoodFriend.position[1], width: 80, height: 50, pnj: childhoodFriend, dialogue: "Je sais ce que tu as fait ! Tu n'as pas honte ?"},
 ];
 let libraryMapDialogueCollisions = [
     // Message porte fermé
-    {x:945, y:440, width: 16, height: 70, pnj: libraryBackDoor, dialogue:"Nécessite une clé pour ouvrir"},
+    {x:945, y:440, width: 16, height: 70, pnj: libraryBackDoor, dialogue:"Nécessite une clé pour ouvrir."},
     {x:librarian.position[0] -30, y:librarian.position[1], width: 80, height: 50, pnj: librarian, dialogue: "Trop bizarre ! J'ai cru voir une bague et un pass ensanglanté..."},
 
 ];
@@ -46,4 +48,3 @@ function drawAllDialogueCollisionsBox() {
         ctx.fillRect(element.x, element.y, element.width, element.height);
     });
 }
-
